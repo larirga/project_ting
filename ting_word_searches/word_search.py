512d519839ce25dd9f9c8da214fb529136ca8b0d
Larissa <larisssa_rga@hotmail.com>
2023-06-15 11:14:49 -0300

    [add] - req7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,19 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    counter = []
+    occrr = []
+    for index in range(len(instance)):
+        row = instance.search(index)
+        for index, line in enumerate(row['linhas_do_arquivo']):
+            if word.lower() in line.lower():
+                occrr.append({'linha': index + 1})
+        if occrr:
+            counter.append({
+                'palavra': word,
+                'arquivo': row['nome_do_arquivo'],
+                'ocorrencias': occrr,
+            })
+
+    return counter
 
 
 def search_by_word(word, instance):
